83cb0d9164e999e47423d9335dea66574134e4f3
Red1an <pronmaxim1@gmail.com>
2026-03-28 15:44:48 +0300

    Add DTOs to Application

---
+++ ReportBroker/ReportBroker.Application/DTOs/CreateReportRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportBroker.Application.DTOs
+{   
+    public class CreateReportRequestDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public Guid ProductId { get; set; }
+        public Guid DesignId { get; set; }
+        public DateOnly PeriodStart { get; set; }
+        public DateOnly PeriodEnd { get; set; }
+    }
+}
---
+++ ReportBroker/ReportBroker.Application/DTOs/ReportStatusDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportBroker.Application.DTOs;
+
+public class ReportStatusDto
+{
+    public Guid RequestId { get; set; }
+    public Guid ReportId { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public double? ConversionRatio { get; set; }
+    public int? PaymentCount { get; set; }
+    public int? ViewCount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+}
