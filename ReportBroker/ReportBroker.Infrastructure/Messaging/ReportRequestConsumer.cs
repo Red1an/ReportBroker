474e01f04985fe9e4621a4c04bdaea79099d19de
Red1an <pronmaxim1@gmail.com>
2026-03-29 16:10:58 +0300

    add integration with RabbitMq

---
+++ ReportBroker/ReportBroker.Infrastructure/Messaging/ReportRequestConsumer.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using ReportBroker.Application.DTOs;
+using ReportBroker.Application.Services;
+
+namespace ReportBroker.Infrastructure.Messaging
+{
+    public class ReportRequestConsumer : IConsumer<ReportRequestMessage>
+    {
+        private readonly CreateReportRequestService _createReqService;
+        private readonly ProcessReportService _processRepService;
+
+        public ReportRequestConsumer(CreateReportRequestService createReqService,  ProcessReportService processRepService)
+        {
+            _createReqService = createReqService;
+            _processRepService = processRepService;
+        }
+
+        public async Task Consume(ConsumeContext<ReportRequestMessage> context)
+        {
+            var message = context.Message;
+
+            var dto = new CreateReportRequestDto
+            {
+                UserId = message.UserId,
+                ProductId = message.ProductId,
+                DesignId = message.DesignId,
+                PeriodStart = message.PeriodStart,
+                PeriodEnd = message.PeriodEnd
+            };
+
+            var request = await _createReqService.ExecuteAsync(dto, context.CancellationToken);
+
+            await _processRepService.ExecuteAsync(request.ReportId, context.CancellationToken);
+        }
+    }
+}
---
+++ ReportBroker/ReportBroker.Infrastructure/Messaging/ReportRequestMessage.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ReportBroker.Infrastructure.Messaging
+{
+    public record ReportRequestMessage(
+        string UserId,
+        Guid ProductId,
+        Guid DesignId,
+        DateOnly PeriodStart,
+        DateOnly PeriodEnd); 
+}
